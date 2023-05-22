5e76b159c4f3a42cdfe6c3217017fda092539d55
Victor <mahluzavictor@gmail.com>
2023-05-22 14:13:25 +0200

    setting.py fix:remove-static-root

--- config/settings.py
+++ config/settings.py
@@ -61,7 +61,7 @@ DEBUG = True
 
 
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -158,10 +158,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
