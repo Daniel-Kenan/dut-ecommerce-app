e2d5b2152a5c7ccc1fd64990e3481734d857ba47
Victor <mahluzavictor@gmail.com>
2023-05-22 13:59:40 +0200

    setting.py fix:static_dir

--- config/settings.py
+++ config/settings.py
@@ -159,6 +159,9 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
