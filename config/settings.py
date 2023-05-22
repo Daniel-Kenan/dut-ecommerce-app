de7d5edd03a8515db599addd2cb336ff8a995fbc
Victor <mahluzavictor@gmail.com>
2023-05-22 14:47:26 +0200

    base.htm fix:logo

--- config/settings.py
+++ config/settings.py
@@ -126,11 +126,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"), 
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
-MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
